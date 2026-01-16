8d4daffdca3d541ef804011f4b8006877960680d
JosephineKa <kamwelajosephinek@gmail.com>
2026-01-16 14:44:51 -0600

    webpage practice1

---
+++ src/main/java/Practice2.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Practice2 {
+public static void main(String[]args){
+
+    WebDriver  driver = new ChromeDriver();
+}
+
+
+
+}
--- src/main/java/SeleniumTestPractice.java
+++ src/main/java/SeleniumTestPractice.java
@@ -2,21 +2,34 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 
 public class SeleniumTestPractice {
     public static void main(String[] args){
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("--start-maximized");
         // creating a new code driver instance
         WebDriver  driver = new ChromeDriver();
 
 //Navigate into the web page
-        driver.get("https://www.saucedemo.com/");
+        driver.get("https://www.google.com/");
 
-        WebElement  searchBox = driver.findElement(By.name("q"));
-
-        searchBox.sendKeys("Selenium");
-
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("facebook");
         searchBox.submit();
+
+        String title = driver.getTitle();
+        if (title.contains("selenium")) {
+            System.out.println("Test Passed");
+        } else {
+            System.out.println("Test Failed");
+        }
+
+       // driver.quit();
+
 
     }
 
